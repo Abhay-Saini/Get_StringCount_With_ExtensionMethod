332f0258f0e17b19c2f01c51331c16177cce1561
abhay <abhay@DESKTOP-2G49AHH>
2022-05-11 00:33:41 +0530

    Add project files.

---
+++ UpperToLower/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_StringCount
+{
+    public static class StringExtension
+    {
+        public static int GetWordCount(this string inputstring)
+        {
+            if (!string.IsNullOrEmpty(inputstring))
+            {
+                string[] strArray = inputstring.Split(' ');
+                return strArray.Count();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myWord = "Welcome to India";
+            int wordCount = myWord.GetWordCount();
+
+            Console.WriteLine("string : " + myWord);
+            Console.WriteLine("Count : " + wordCount);
+
+            Console.Read();
+        }
+    }
+}
